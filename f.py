87904ab018021d241907d04cb87084105281c135
suryapoojasree <82646420+suryapoojasree@users.noreply.github.com>
2021-04-16 22:28:22 +0530

    Add files via upload
    this file is about format string

---
+++ f.py
@@ -0,0 +1,18 @@
+my_name = 'pooja sree'
+my_age = 19
+my_height = 60
+my_weight = 80
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"She's {my_height} inches tall.")
+print(f"She's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} depending on the coffee.")
+
+#this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight 
+print(f"If I add {my_age}, {my_height},and{my_weight} I get {total}.")
